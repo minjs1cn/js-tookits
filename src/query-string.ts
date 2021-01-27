233a58b9337271b4ca57de980e03675d61a160ae
minjs1cn <303890562@qq.com>
2021-01-27 12:10:37 +0800

    fix qs

--- src/query-string.ts
+++ src/query-string.ts
@@ -1,6 +1,44 @@
-import { parse as Parse, stringify } from 'query-string'
+import { encode, decode } from './url'
 
-const parse = (str: string): { [index: string]: string | null } => Parse(str) as { [index: string]: string }
+interface Result { [index: string]: string | undefined }
+
+function parse(query: string): Result {
+
+	// Create an object with no prototype
+	const ret = Object.create(null);
+
+	if (typeof query !== 'string') {
+		return ret;
+	}
+
+	query = query.trim().replace(/^[?#&]/, '');
+
+	if (!query) {
+		return ret;
+  }
+  let kvs
+
+  return query.split('&').reduce((res, kv) => {
+    kvs = kv.split('=')
+    res[kvs[0]] = decode(kvs[1])
+    return res
+  }, {} as Result)
+}
+
+function stringify(params: Result) {
+  let value
+  let v
+
+  return Object.keys(params)
+    .map(key => {
+      v = params[key]
+
+      value = v === null || v === undefined ? '' : v
+
+      return `${key}=${encode(value)}`
+    })
+    .join('&')
+}
 
 export {
   parse,
